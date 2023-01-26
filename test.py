de11477dc6c8e4877699e3ac7f3c4ce01fc818a0
Andrzej Dulny <andrzej.dulny@uni-wuerzburg.de>
2023-01-26 12:10:03 +0100

    Removed test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-from src.dataset import DynaBenchBase, DynaBenchGraph
-
-ds = DynaBenchGraph(equation="wave", lookback=8, rollout=16, support="cloud", task="forecast")
-
-x, y, p = ds[0]
-
-print(x)
