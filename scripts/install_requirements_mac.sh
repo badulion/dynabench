71631bdc58700d66ef5153f4fc401e324814fd0b
Anna Krause <anna.krause@uni-wuerzburg.dex>
2023-02-10 11:23:42 +0100

    Create install file for macbook

---
+++ scripts/install_requirements_mac.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+pip install -r requirements.txt
+pip install pyg-lib torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.13.0+cpu.html
