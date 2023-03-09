fe4ce415506907185e90b327b861c8468e753eae
Andrzej Dulny <andrzej.dulny@uni-wuerzburg.de>
2023-03-09 17:31:59 +0100

    Bumped cuda version

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 pip install -r requirements.txt
-pip install pyg-lib torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.13.0+cu116.html
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv torch_geometric -f https://data.pyg.org/whl/torch-1.13.0+cu116.html
