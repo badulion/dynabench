1072c68d7b44c6d780100f34aafcbafb38f9f27f
Andrzej Dulny <andrzej.dulny@uni-wuerzburg.de>
2023-10-18 15:59:49 +0200

    Added download script for fetching from Wuedata

--- src/setup.py
+++ src/setup.py
@@ -1,12 +1,18 @@
 import setuptools 
 
 setuptools.setup(
-      version='0.1.0',
-      description='Benchmark dataset for learning dynamical systems from data',
       name="dynabench",
-      packages=setuptools.find_packages('.'),
+      version='0.2.0',
+      description='Benchmark dataset for learning dynamical systems from data',
+      author='Andrzej Dulny',
+      author_email='andrzej.dulny@protonmail.com',
+      licence='',
+      packages=['dynabench'],
       package_dir={'': '.'},
+      package_data={'dynabench': ['config/*.yaml']},
       install_requires=[
-        "numpy>=1.23.5", "py-pde>=0.27.1", "torch>=2.0.1", "scipy>=1.2.0", "torchdata>=0.6.0", 
-      ]
-     )
+        "requests", "tqdm", "numpy>=1.23.5", "h5py>=3.9.0"
+      ],
+      extras_require={
+        "dynabench": ["dedalus>=3"],
+    })
