bc4b3223a8b397ea457768c7462c9bc3dc3d4292
Andrzej Dulny <andzej.dulny@gmail.com>
2024-05-06 11:27:20 +0200

    Created class to represent diff operators

--- src/dynabench/solver/diff.py
+++ src/dynabench/solver/diff.py
@@ -1,42 +1,74 @@
 import numpy as np
 from findiff import coefficients
+from scipy.signal import convolve2d
 
-def differential_operator(derivative: str = "u_x_0_y_0", acc: int = 2, dx: float = 1, dy: float = 1) -> np.ndarray:
-
+# ToDo: implement boundary conditions
+class DifferentialOperator(object):
     """
-        For a given string representation of a derivative, return the corresponding differential operator 
-        as a numpy array. The derivative string is formatted as follows: u_x_i_y_j, where i and j are the
-        order of the derivative with respect to x and y, respectively.
+        Represents a differential operator by a finite difference method. The internal representation 
+        of the operator is a numpy array and the operator is applied by running a convolution.
+        See `NeuralPDE: Modelling Dynamical Systems from Data <https://arxiv.org/abs/2111.07671>`_ for more details.
 
-        Parameters:
-        -----------
-        derivative : str
+        The coefficients of the operator are computed using the `findiff <https://pypi.org/project/findiff/>`_ package.
+
+        Parameters
+        ----------
+        derivative : str, default "u_x_0_y_0"
             The string representation of the derivative.
-        acc : int
-            The accuracy of the derivative.
-        dx : float
+        acc : int, default 2
+            The accuracy with which to calculate the derivative.
+        dx : float, default 1
             The spacing between grid points in the x direction.
-        dy : float
+        dy : float, default 1
             The spacing between grid points in the y direction.
-
-        Returns:
-        --------
-        np.ndarray
-            The differential operator as a numpy array.
     """
 
-    # parse orders
-    derivative = derivative.split("_")
-    order_x, order_y = int(derivative[2]), int(derivative[4])
+    def __init__(self, derivative: str = "u_x_0_y_0", acc: int = 2, dx: float = 1, dy: float = 1):
+        self.derivative = derivative
+        self.acc = acc
+        self.dx = dx
+        self.dy = dy
 
-    if order_x == 0:
-        c_x = np.array([[1]])
-    else:
-        c_x = coefficients(deriv=order_x, acc=acc)['center']['coefficients'].reshape(-1, 1)/dx**order_x
+        self.coeffs = self._calculate_coeffs(derivative, acc, dx, dy)
 
-    if order_y == 0:
-        c_y = np.array([[1]])
-    else:
-        c_y = coefficients(deriv=order_y, acc=acc)['center']['coefficients'].reshape(1, -1)/dy**order_y
+    def _calculate_coeffs(self, derivative_str: str, acc: int, dx: float, dy: float) -> np.ndarray:
+        """
+            Calculate the coefficients of the differential operator.
 
-    return c_x@c_y
+            Parameters
+            ----------
+            derivative_str : str
+                The string representation of the derivative.
+            acc : int
+                The accuracy with which to calculate the derivative.
+            dx : float
+                The spacing between grid points in the x direction.
+            dy : float
+                The spacing between grid points in the y direction.
+
+            Returns
+            -------
+            np.ndarray
+                The coefficients of the differential operator.
+        """
+        # parse orders
+        derivative = derivative_str.split("_")
+        order_x, order_y = int(derivative[2]), int(derivative[4])
+
+        if order_x == 0:
+            c_x = np.array([[1]])
+        else:
+            c_x = coefficients(deriv=order_x, acc=acc)['center']['coefficients'].reshape(-1, 1)/dx**order_x
+
+        if order_y == 0:
+            c_y = np.array([[1]])
+        else:
+            c_y = coefficients(deriv=order_y, acc=acc)['center']['coefficients'].reshape(1, -1)/dy**order_y
+
+        return c_x@c_y
+
+    def __str__(self):
+        return f"{self.derivative}"
+
+    def __call__(self, u: np.ndarray) -> np.ndarray:
+        return convolve2d(u, self.coeffs, mode="same", boundary="wrap")
