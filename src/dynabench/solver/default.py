338aeefe8b5ba88de0b24dcabdfffd342d8fdf72
Andrzej Dulny <andzej.dulny@gmail.com>
2024-05-06 18:07:06 +0200

    added make_eq_callable to solver

--- src/dynabench/solver/default.py
+++ src/dynabench/solver/default.py
@@ -1,4 +1,6 @@
-
+import scipy.integrate as spi
+import dynabench.equation
+from .diff import DifferentialOperator
 
 class FinDiffSolver(object):
     """
@@ -35,6 +37,28 @@ class FinDiffSolver(object):
         """
         return len(self._variables)
     
+    def _make_equation_callable(self, equation: dynabench.equation.BaseEquation) -> callable:
+        """
+            Convert the RHS of the equation string to a callable function.
+
+            Parameters
+            ----------
+            equation_rhs : str
+                The string representation of the equation.
+
+            Returns
+            -------
+            callable
+                The function representing the equation.
+        """
+        diff_operators_dict = {
+            "u_x_0_y_0": DifferentialOperator(derivative="u_x_0_y_0"),
+            "u_x_1_y_0": DifferentialOperator(derivative="u_x_1_y_0"),
+        }
+
+        equation = "u_x_0_y_0(u) + u_x_1_y_0(u)"
+        return lambda u, t: eval(equation, {'u': u, **diff_operators_dict})
+    
     def solve(self, *args, **kwargs):
         """
             Solve the equation.
@@ -44,4 +68,5 @@ class FinDiffSolver(object):
             np.ndarray
                 The solution of the equation.
         """
-        raise NotImplementedError("The solve method must be implemented in the subclass.")	
+        def f(u, t):
+            return 0
