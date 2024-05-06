72ee57509d0c4f5c92dfe0b169809b12f37e17a1
Andrzej Dulny <andzej.dulny@gmail.com>
2024-05-06 21:09:22 +0200

    Added neuralpde to module _all_

--- src/dynabench/model/__init__.py
+++ src/dynabench/model/__init__.py
@@ -4,6 +4,7 @@ Module containing the models used in the DynaBench benchmark.
 
 from ._grid.cnn import CNN
 from ._grid.resnet import ResNet
+from ._grid.neuralpde import NeuralPDE
 
 
-__all__ = ['CNN', 'ResNet']
+__all__ = ['CNN', 'ResNet', 'NeuralPDE']
