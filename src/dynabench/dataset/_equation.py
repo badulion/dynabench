9b939c4375f1efdc61427841462755d48d7e311f
Andrzej Dulny <andzej.dulny@gmail.com>
2024-07-03 16:48:26 +0200

    Add documentation for Equation Moving Window Iterator

--- src/dynabench/dataset/_equation.py
+++ src/dynabench/dataset/_equation.py
@@ -4,6 +4,20 @@ import numpy as np
 from typing import List
 
 class EquationMovingWindowIterator:
+    """
+        Iterator for arbitrary equations generated using the dynabench solver. Each sample returned by the __getitem__ method is a tuple of 
+        (data_input, data_target, points), where data_input is the input data of shape (L, F, H, W), data_target is the target data of shape (R, F, H, W), and points are the points in the grid of shape (H, W, 2).
+        In this context L corresponds to the lookback parameter and R corresponds to the rollout parameter. H and W are the height and width of the grid, respectively. F is the number of variables in the equation system.
+
+        Parameters
+        ----------
+        data_path : str
+            Path to the data file in h5 format.
+        lookback : int
+            Number of time steps to look back. This corresponds to the L parameter.
+        rollout : int
+            Number of time steps to predict. This corresponds to the R parameter.
+    """
     def __init__(
             self,
             data_path: str,
@@ -17,6 +31,7 @@ class EquationMovingWindowIterator:
 
     def __len__(self):
         with h5py.File(self.data_path, "r") as f:
+            print(f.keys())
             return len(f['times']) - self.lookback - self.rollout + 1
     
     def __getitem__(self, index):
@@ -33,3 +48,22 @@ class EquationMovingWindowIterator:
         points = np.stack([X, Y], axis=-1)
 
         return data_input, data_target, points
+    
+    def get_full_simulation_data(self):
+        """
+            This method returns the full simulation data from the data file, along with the points in the grid.
+            
+            Returns
+            -------
+            np.ndarray, np.ndarray
+                The data and the points. The data has shape (T, F, H, W) and the points have shape (H, W, 2), where T is the number of time steps, F is the number of variables, H and W are the height and width of the grid, respectively.
+        """
+        with h5py.File(self.data_path, "r") as f:
+            data = f['data'][:]
+            x_coords = f['x_coords'][:]
+            y_coords = f['y_coords'][:]
+        
+        X, Y = np.meshgrid(x_coords, y_coords)
+        points = np.stack([X, Y], axis=-1)
+
+        return data, points
