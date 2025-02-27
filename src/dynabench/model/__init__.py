5ef057999de7ae394a4260984cc93194c718752f
Andrzej Dulny <andzej.dulny@gmail.com>
2025-02-27 11:02:02 +0100

    Remove PointTransformerV3 from model module

--- src/dynabench/model/__init__.py
+++ src/dynabench/model/__init__.py
@@ -6,7 +6,7 @@ from ._grid.cnn import CNN
 from ._grid.resnet import ResNet
 from ._grid.neuralpde import NeuralPDE
 
-from .point.point_transformer import PointTransformerV1, PointTransformerV3
+from .point.point_transformer import PointTransformerV1
 
 
-__all__ = ['CNN', 'ResNet', 'NeuralPDE', 'PointTransformerV1', 'PointTransformerV3']
+__all__ = ['CNN', 'ResNet', 'NeuralPDE', 'PointTransformerV1']
