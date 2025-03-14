a3f6dbffa736983926a7d2dcdee1eb023073841e
Andrzej Dulny <andzej.dulny@gmail.com>
2025-03-14 17:35:37 +0100

    Update one more example

--- examples/example_point_transformer_v3.py
+++ examples/example_point_transformer_v3.py
@@ -45,6 +45,7 @@ advection_test_iterator = DynabenchIterator(split="test",
                                           structure='cloud',
                                           resolution='low',
                                           lookback=1,
+                                          squeeze_lookback_dim=True,
                                           rollout=16)
 
 test_loader = DataLoader(advection_test_iterator, batch_size=1, shuffle=False)
@@ -53,7 +54,7 @@ model.eval()
 
 loss_values = []
 for i, (x, y, p) in enumerate(test_loader):
-    x, y, p = x[:,0].float(), y.float(), p.float() # only use the first channel and convert to float32
+    x, y, p = x.float(), y.float(), p.float() # only use the first channel and convert to float32
     y_pred = model(x, p, t_eval=range(1,17))
     loss = criterion(y_pred, y)
     loss_values.append(loss.item())
