5783fd2d254d947609796844636b230285d3a883
Andrzej Dulny <andzej.dulny@gmail.com>
2025-04-17 10:57:26 +0200

    patch FNO to include in and out channels

--- src/dynabench/model/__init__.py
+++ src/dynabench/model/__init__.py
@@ -4,6 +4,7 @@ Module containing the models used in the DynaBench benchmark.
 
 from ._grid.cnn import CNN
 from ._grid.resnet import ResNet
+from ._grid._neural_operator import FourierNeuralOperator
 from ._grid.neuralpde import NeuralPDE
 
 from .point.point_transformer import PointTransformerV1
